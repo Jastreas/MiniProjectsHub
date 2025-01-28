0575d30554b2c05232a1f217ce84af3a5f6eedee
AverageProgrammerDude <149157678+Jastreas@users.noreply.github.com>
2025-01-28 18:24:00 +0100

    writed checkInputMethod and completed the program

--- C++/Random Exercises/CPP-WriteUserInputIntoFile/main.cpp
+++ C++/Random Exercises/CPP-WriteUserInputIntoFile/main.cpp
@@ -8,8 +8,8 @@ Save as file
 #include <fstream>
 using namespace std;
 
-void checkInput(){
-    //Code
+bool checkInput(string& input){
+    return input.empty();
 }
 
 
@@ -17,12 +17,18 @@ int main(){
 
     ofstream file("test.txt");
     //file.open("test.txt");
-    string text = "NULL";
+    string text = "";
     cout << "Please introduce whatever you want to save as a file." << endl;
     cin >> text;
-    file << text;
-    file.close();
 
+    if(checkInput(text) != true){
+        file << text;
+    } else {
+        file << "Oh No! The input was empty D:";
+        cout << "There was an error writing the input...";
+    }
+
+    file.close();
 
 return 0;
 }
